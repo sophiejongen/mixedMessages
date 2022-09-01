7d9cce7bbd65497e64c4325fd6aa77e7eff98199
Sophie <sophie.jongen98@gmail.com>
2022-09-01 14:12:27 +0100

    3 random components have been added

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
-console.log('hello');
-console.log(8)
+// These are my 3 categories 
+
+const color = ['red', 'pink', 'green', 'blue', 'purple', 'yellow', 'orange', 'brown'];
+
+const character = ['baby', 'dog', 'kangaroo', 'snake', 'elephant', 'mouse', 'plant', 'shark'];
+
+const object = ['hat', 'balloon', 'phone', 'watch', 'headphones', 'wig', 'gloves', 'icecream cone'];
+
+// now I need a random number between 0-7 
+
