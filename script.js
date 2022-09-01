c3cc3b500e6c8c31f6c0d67f98fafd55043eb1f8
Sophie <sophie.jongen98@gmail.com>
2022-09-01 14:33:57 +0100

    all changes have been made and now generates random lucky charms

--- script.js
+++ script.js
@@ -4,7 +4,19 @@ const color = ['red', 'pink', 'green', 'blue', 'purple', 'yellow', 'orange', 'br
 
 const character = ['baby', 'dog', 'kangaroo', 'snake', 'elephant', 'mouse', 'plant', 'shark'];
 
-const object = ['hat', 'balloon', 'phone', 'watch', 'headphones', 'wig', 'gloves', 'icecream cone'];
+const object = ['a hat', 'a balloon', 'a phone', 'a watch', 'headphones', 'a wig', 'gloves', 'an icecream cone'];
 
 // now I need a random number between 0-7 
 
+const generateColor=Math.floor(Math.random() * 8);
+const generateCharacter=Math.floor(Math.random() * 8);
+const generateObject=Math.floor(Math.random() * 8);
+
+//console.log(generateColor);
+
+let colorChoice = color[generateColor];
+let characterChoice = character[generateCharacter];
+let objectChoice = object[generateObject];
+
+//console.log(colorChoice); 
+console.log(`Your lucky charm is a ${colorChoice} ${characterChoice} with ${objectChoice}.`);
